757334fa0cb43fe1f7de54d4c0aba51d5466e24b
Hi <dengwq1997@163.com>
2023-06-09 15:51:30 +0800

    Update Searchfiles_move.py

--- Searchfiles/Searchfiles_move.py
+++ Searchfiles/Searchfiles_move.py
@@ -10,8 +10,8 @@ def search_and_move_files(source_folder, target_folder, extensions):
                 target_path = os.path.join(target_folder, file)
                 shutil.move(file_path, target_path)
 
-source_folder = "D:\\邓伟清\\D盘\\技术部邓伟清\\08临时存放\\工作微信文件缓存\\新建文件夹"  # 替换为你要搜索的文件夹路径
-target_folder = "D:\\邓伟清\\D盘\\技术部邓伟清\\08临时存放\\工作微信文件缓存\\文件"  # 替换为你要将文件移动到的目标文件夹路径
+source_folder = "D:\\08临时存放\\工作微信文件缓存\\新建文件夹"  # 替换为你要搜索的文件夹路径
+target_folder = "D:\\08临时存放\\工作微信文件缓存\\文件"  # 替换为你要将文件移动到的目标文件夹路径
 extensions = ["xlsx", "pdf", "docx", "pptx", "doc", "PDF" ,"xls"]  # 要搜索的文件扩展名列表
 
 search_and_move_files(source_folder, target_folder, extensions)
